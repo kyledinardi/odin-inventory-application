4f1a512f12cd2a8a5499eb58be5e125d8355189f
kyledinardi <{ID}+{username}@users.noreply.github.com>
2024-05-21 14:35:50 -0400

    Add middleware

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+require('dotenv').config();
 const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
+const debug = require('debug')('express-locallibrary-tutorial:app');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+app.use(helmet());
+
+const mongoDB = process.env.MONGODB_URI;
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch((err) => debug(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +37,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
